7747cafc69b2d0c10e2581756a603718135a3783
Daniel Gomez <Daniel@10.5.4.79>
2018-10-12 16:03:37 -0500

    prueba 2

--- DesarrolloSoftware/src/desarrollosoftware/DesarrolloSoftware.java
+++ DesarrolloSoftware/src/desarrollosoftware/DesarrolloSoftware.java
@@ -15,7 +15,7 @@ public class DesarrolloSoftware {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        hiola
     }
     
 }
